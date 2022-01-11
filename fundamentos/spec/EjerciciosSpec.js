80aa8325389ed8dfacfe37fe1a5b170567a2ada9
David Iglesias <dvd.iglesias@outlook.com>
2022-01-11 18:32:33 +0100

    angular proyect

--- fundamentos/spec/EjerciciosSpec.js
+++ fundamentos/spec/EjerciciosSpec.js
@@ -1,5 +1,5 @@
 
-import {randomFromRange, getRepeatedArray, isValidNif, isPalindrome} from "../ejercicios/ejercicios1.js";
+import {randomFromRange, getRepeatedArray, getFirstPrimes, isValidNif, isPalindrome} from "../ejercicios/ejercicios1.js";
 
 describe("Ejercicios Bloque 1", function(){
 
@@ -62,11 +62,11 @@ describe("Ejercicios Bloque 1", function(){
         })
 
         it(`Range as String`, function(){
-            expect(typeof intervalTest('100', -4)).toBe('number');
+            expect(intervalTest('100', -4)).toBeInstanceOf(Number);
         });
 
         it(`From as String`, function(){
-            expect(typeof intervalTest(100, '-30')).toBe('number');
+            expect(intervalTest(100, '-30')).toBeInstanceOf(Number);
         });
 
         it(`3 arguments`, function(){
@@ -131,17 +131,50 @@ describe("Ejercicios Bloque 1", function(){
         });
     });
 
+    describe("Ejercicio 4: List of prime numbers", function(){
+        let trustedPrimes = [
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+            101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+            211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
+            307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
+            401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,
+            503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
+            601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
+            701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+            809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
+            907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997
+        ];
+
+        it(`2 is the first prime`, function(){
+            let firstPrime = getFirstPrimes(1)[0];
+            expect(firstPrime).toBe(2);
+        })
+
+        // [2, 10].forEach(value =>{
+        //     it(`Length is right: length = ${value}`, function(){
+        //         false();
+        //     })
+        // });
+
+        // [2, 10, 100, 25, 18].forEach(size => {
+        //     it(`Length is right: length = ${size}`, function(){
+        //         let firstPrimes = getFirstPrimes(size);
+        //         expect(firstPrimes.length).toBe(size);
+        //     })
+        // });
+    });
+
     describe("Ejercicio 5: NIF Validator", function(){
 
         ['12345678Z', '12345678-Z', '12345678z', '12345678-z', '0T', '00010X'].forEach(nif =>{
             it(`OK format: nif = ${nif}`, function(){
-                expect(isValidNif(nif)).toBe(true);
+                expect(isValidNif(nif)).toBeTrue();
             })
         });
 
         ['Z12345678', 'Random', 77, '000000000T', '123456789B', '00000T000', undefined, null].forEach(nif =>{
             it(`KO format: nif = ${nif}`, function(){
-                expect(isValidNif(nif)).toBe(false);
+                expect(isValidNif(nif)).toBeFalse();
             })
         });
     });
